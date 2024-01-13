7c9786dc7b4cf0b486428072adc313706dcdce69
Andrew Rowatt <a.j.rowatt@massey.ac.nz>
2024-01-14 11:07:47 +1300

    Initial version using swiper

--- H5P.MUNoticesCarousel/main.js
+++ H5P.MUNoticesCarousel/main.js
@@ -42,65 +42,71 @@ H5P.MUNoticesCarousel = (function ($) {
 		$container.addClass("h5p-notices-onecol");
 	}
 
-	var html = '<div class="alert alert-info">Alert!</div>';
+	var html = '';
 
-	html += '<div class="carousel slide" data-ride="carousel" data-interval="7000" id="stream-dashboard-notices">';
-	html += '<div class="carousel-inner" role="listbox">';
-
-	var buttonHTML = '<div class="carousel-indicators js-only">'; /* Start slide button markup */
-	var buttonIndex = 0;
-
-	var firstItem = true;
+	html += '<div id="stream-dashboard-notices">';
+	html += '<div class="swiper">';
+	html += '<div class="swiper-wrapper">';
 
 	for(notice of this.options.notices) {
 		if(notice.visible) { /* Skip notices that are not visible */
-			/* First item is always marked as active  */
-			html += `<div class="carousel-item ${firstItem ? " active" : ""}"><div class="item">`;
-
-			buttonHTML += `<button data-target="#stream-dashboard-notices" data-slide-to="${buttonIndex}" class="${firstItem ? "active" : ""}" aria-label="slide-0" aria-current="true">${buttonIndex + 1}</button>`;
-			buttonIndex+=1;
-
-			firstItem = false;
+			var hasImage = notice.image && notice.image.path;
+			html += `<div class="swiper-slide"><div class="item${hasImage?" item__has_image":""}">`;
 
 			html += `<div><div class="item__heading">`;
 
 			html += `<h6>${notice.title}</h6><div class="item__info item__info_update">${notice.update}</div></div>`;
 			html += `<div class="item__content">${notice.content}</div>`;
 
-			if(notice.moreinformation) {
-				html += `<div class="item__more_info">`;
-				if(notice.moreinformation.startsWith('http')) {
-					html += `<a href="${notice.moreinformation}" target="_blank">More information</a>`;
-				} else {
-					html += `By <a href="mailto:${notice.moreinformation}" target="_blank">${notice.owner}</a>`;
-				}
-				html += '</div>';	
+			html += `<div class="item__more_info">`;
+			if(notice.moreinformation && notice.moreinformation.startsWith('http')) {
+				html += `<a href="${notice.moreinformation}" target="_blank">More information</a>`;
+			} else {
+				html += `By <a href="mailto:${notice.owneremail}" target="_blank">${notice.owner}</a>`;
+			}
+			html += '</div>'; /* End .item__more_info */
+			
+			html += '</div>'; /* Anonymous div */
+
+			if (hasImage) {
+				html += `<div class="img" style="background-image:url(${H5P.getPath(notice.image.path, this.id)});"></div>`;
 			}
 
-			html += '</div>';
-
-			if (notice.image && notice.image.path) {
-				html += `<div><img alt="" src="${H5P.getPath(notice.image.path, this.id)}"></div>`;
-			}
-
-			html += '</div></div>'; /* End item and carousel-item */
+			html += '</div></div>'; /* End .item and .swiper-slide */
 		}
 	}
-	html += '</div>'; /* carousel-inner */
+	html += '</div>'; /* End .swiper-wrapper */
 
-	/* Standard left and right controls */
-	html += '<a class="carousel-control carousel-control-prev" href="#stream-dashboard-notices" role="button" data-slide="prev"><span class="carousel-control" aria-hidden="true"><i class="icon fa fa-chevron-left fa-fw " aria-hidden="true"></i></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" href="#stream-dashboard-notices" role="button" data-slide="next"><span class="carousel-control" aria-hidden="true"><i class="icon fa fa-chevron-right fa-fw " aria-hidden="true"></i></span><span class="sr-only">Next</span></a>';
-
-	/* End slide button markup */
-	buttonHTML += '</div>';
-	html += buttonHTML;
-
-	html += '</div>'; /* carousel */
+	html += '<div class="swiper-pagination"></div>';
+	html += '<div class="swiper-button-prev"></div><div class="swiper-button-next"></div>';
+	html += '</div></div>'; /* End .swiper and #stream */
 	
 	$container.append(html);
 	
-	//console.log(this);
-	//console.log($container);
+	$(document).ready(function () {
+		// https://swiperjs.com/swiper-api#initialize-swiper
+
+		const swiper = new Swiper('.swiper', {
+			direction: 'horizontal',
+			autoplay: {
+				delay: 5000,
+				pauseOnMouseEnter: true,
+				disableOnInteraction: true,
+			},
+			loop: true,
+		  
+			// We need pagination
+			pagination: {
+			  el: '.swiper-pagination',
+			},
+		  
+			// Navigation arrows
+			navigation: {
+			  nextEl: '.swiper-button-next',
+			  prevEl: '.swiper-button-prev',
+			},
+		  });		
+	});
   };
   
   C.prototype.getCurrentState = function () {
@@ -127,48 +133,3 @@ H5P.MUNoticesCarousel = (function ($) {
   };
   return C;
 })(H5P.jQuery);
-
-/*
-<p>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11.0.5/swiper-bundle.min.js" type="text/javascript"></script>
-</p>
-<p><link href="https://cdn.jsdelivr.net/npm/swiper@11.0.5/swiper-bundle.min.css" rel="stylesheet" /></p>
-<!-- Slider main container -->
-<div class="swiper" style="height: 300px;"><!-- Additional required wrapper -->
-<div class="swiper-wrapper"><!-- Slides -->
-<div class="swiper-slide">Slide 1</div>
-<div class="swiper-slide">Slide 2</div>
-<div class="swiper-slide">Slide 3</div>
-</div>
-<!-- If we need pagination -->
-<div class="swiper-pagination"></div>
-<!-- If we need navigation buttons -->
-<div class="swiper-button-prev"></div>
-<div class="swiper-button-next"></div>
-<!-- If we need scrollbar -->
-<div class="swiper-scrollbar"></div>
-</div>
-
-
-const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
-});
-*/
